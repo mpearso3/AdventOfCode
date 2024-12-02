b4f6861a4c5316d67e6d9acdcde03b014e8f5ed9
Matt Pearson <pearson.matt345@gmail.com>
2024-12-01 22:00:29 -0700

    Stubbed out day 2 before it starts

---
+++ 2024/Day_2/main.py
@@ -0,0 +1,20 @@
+
+class Day2Solver:
+    def __init__(self):
+        pass
+
+def main():
+    '''
+    Example input:
+    '''
+
+    day2Solver = Day2Solver()
+
+    day2Solver.solve_part_1("input_simple_1.txt")
+    # day1Solver.solve_part_1("input.txt")
+
+    # day1Solver.solve_part_2("input_simple_1.txt")
+    # day1Solver.solve_part_2("input.txt")
+
+if __name__ == "__main__":
+    main()
