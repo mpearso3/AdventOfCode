cd2adfe35efe9266a1292f29d46aa7215282bb92
Matt Pearson <pearson.matt345@gmail.com>
2024-12-02 23:31:50 -0700

    Fixed part 2 with a much cleaner solution

--- 2024/Day_3/main.py
+++ 2024/Day_3/main.py
@@ -33,46 +33,30 @@ class Day3Solver:
         print(f"{in_file_name}: sum: {multiplies}")
 
     def get_pattern_indexes(self, pattern, input):
-        matches = []
+        matches = {}
         for match in re.finditer(pattern, input):
-            matches.append(match.start())
+            start_index = match.start()
+            matches[start_index] = pattern
         return matches
 
-    def remove_invalid_pairs(self, dont_matches, do_matches, mul_matches):
+    def remove_invalid_pairs(self, sorted_matches):
         valid_indexes = []
 
-        current_dont_index = dont_matches.pop(0)
-        current_do_index = do_matches.pop(0)
-
+        mul_count = 0
         enabled = True
-        for i, index in enumerate(mul_matches):
-            if index < current_do_index and index < current_dont_index:
+        for start_index, pattern in sorted_matches.items():
+            if 'mul' in pattern:
                 if enabled:
-                    valid_indexes.append(i)
-            elif index > current_do_index and index < current_dont_index:
-                enabled = True
-                valid_indexes.append(i)
-                if len(do_matches) > 0:
-                    current_do_index = do_matches.pop(0)            
-            elif index > current_dont_index and index < current_do_index:
+                    valid_indexes.append(mul_count)
+                mul_count += 1
+            elif "don't" in pattern:
                 enabled = False
-                if len(dont_matches) > 0:
-                    current_dont_index = dont_matches.pop(0)            
-            else: # greater than both
-                if current_dont_index > current_do_index:
-                    enabled = False
-                else:
-                    enabled = True
-                    valid_indexes.append(i)
-
-                if len(dont_matches) > 0:
-                    current_dont_index = dont_matches.pop(0)            
-                if len(do_matches) > 0:
-                    current_do_index = do_matches.pop(0)            
+            elif 'do' in pattern:
+                enabled = True
 
         return valid_indexes
 
-    def solve_part_2(self, in_file_name):
+    def get_input_as_single_string(self, in_file_name):
         f = open(in_file_name, 'r')
         lines = f.readlines()
         f.close()
@@ -81,15 +65,31 @@ class Day3Solver:
         for line in lines:
             sanitized_line = line.strip().rstrip()
             full_input += sanitized_line
+        
+        return full_input
+
+    def combine_matches(self, dont_matches, do_matches, mul_matches):
+        matches = {}
+        matches.update(dont_matches)
+        matches.update(do_matches)
+        matches.update(mul_matches)
+
+        sorted_matches = dict(sorted(matches.items()))
+
+        return sorted_matches
+
+    def solve_part_2(self, in_file_name):
+        full_input = self.get_input_as_single_string(in_file_name)
 
         dont_matches = self.get_pattern_indexes("don't\(\)", full_input)
         do_matches = self.get_pattern_indexes("do\(\)", full_input)
         mul_matches = self.get_pattern_indexes("mul\((\d+),(\d+)\)", full_input)
 
+        sorted_matches = self.combine_matches(dont_matches, do_matches, mul_matches)
+        valid_multiply_pairs = self.remove_invalid_pairs(sorted_matches)
+
         multiply_pairs = []
         self.extract_valid_multiply_statements(multiply_pairs, full_input)
-
-        valid_multiply_pairs = self.remove_invalid_pairs(dont_matches, do_matches, mul_matches)
 
         pairs = []
         for i in valid_multiply_pairs:
@@ -99,6 +99,15 @@ class Day3Solver:
         print(f"{in_file_name}: sum: {multiplies}")
 
 def main():
+    '''
+    Correct answers to files:
+
+    input_simple_1.txt: sum: 161
+    input.txt: sum: 162813399
+    input_simple_2.txt: sum: 48
+    input_simple_3.txt: sum: 54
+    input.txt: sum: 53783319
+    '''
     day3Solver = Day3Solver()
 
     day3Solver.solve_part_1("input_simple_1.txt")
