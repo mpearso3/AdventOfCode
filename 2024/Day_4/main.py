2078d485619ad25808896eee34a39ed4370771d7
Matt Pearson <pearson.matt345@gmail.com>
2024-12-03 21:54:07 -0700

    Added stubs for day 4

---
+++ 2024/Day_4/main.py
@@ -0,0 +1,20 @@
+class Day4Solver:
+    def solve_part_1(self, in_file_name):
+        answer = 0
+        print(f"{in_file_name}: answer {answer}")
+
+    def solve_part_2(self, in_file_name):
+        answer = 0
+        print(f"{in_file_name}: answer {answer}")
+
+def main():
+    day4Solver = Day4Solver()
+
+    day4Solver.solve_part_1("input_simple_1.txt")
+    day4Solver.solve_part_1("input.txt")
+
+    day4Solver.solve_part_2("input_simple_1.txt")
+    day4Solver.solve_part_2("input.txt")
+
+if __name__ == "__main__":
+    main()
