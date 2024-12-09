730f95713a2fd71c2feb51e25bd90bc9b1565718
Matt Pearson <pearson.matt345@gmail.com>
2024-12-08 23:16:40 -0700

    Finished part 2 of day 9

--- 2024/Day_9/main.py
+++ 2024/Day_9/main.py
@@ -63,12 +63,87 @@ class Day9Solver:
 
         return compressed
 
+    def get_block_size(self, compressed_blocks, id):
+        size = 0
+
+        found_id = False
+        for i in range(len(compressed_blocks) - 1, -1, -1):
+            if compressed_blocks[i] == id:
+                found_id = True
+                size += 1
+            else:
+                if found_id:
+                    break
+
+        return size
+
+    def get_block_sizes(self, compressed_blocks, block_ids):
+        block_sizes = {}
+        for id in block_ids:
+            block_sizes[id] = self.get_block_size(compressed_blocks, id)
+
+        return block_sizes
+
+    def get_block_ids(self, compressed):
+        block_ids = {}
+
+        for i, item in enumerate(compressed):
+            if item != self.free_character:
+
+                if item not in block_ids:
+                    block_ids[item] = {}
+                block_ids[item]['id_index'] = i
+
+        block_ids = dict(sorted(block_ids.items(), key=lambda item: item[0], reverse=True))
+
+        return block_ids
+
+    def get_free_spaces(self, compressed):
+        free_space = []
+
+        count = 0
+        start_index = 0
+        for i, item in enumerate(compressed):
+            if item == self.free_character:
+                if count == 0:
+                    start_index = i
+                count += 1
+            else:
+                if count > 0:
+                    free_space.append({'start_index': start_index, 'size': count})
+                count = 0
+
+        return free_space
+
+    def compress_blocks_with_whole_files(self, block_expansion):
+        compressed = list(block_expansion)
+
+        block_ids = self.get_block_ids(compressed)
+        block_sizes = self.get_block_sizes(compressed, block_ids)
+
+        for id in block_ids:
+            free_spaces = self.get_free_spaces(compressed)
+
+            id_index = block_ids[id]['id_index']
+            block_size = block_sizes[id]
+
+            for free_space in free_spaces:
+                start_index = free_space['start_index']
+                size = free_space['size']
+
+                if start_index < id_index:
+                    if size >= block_size:
+                        for i in range(block_size):
+                            self.swap_elements(compressed, start_index + i, id_index - i)
+
+        return compressed
+
     def filesystem_checksum(self, compressed_block):
         checksum = 0
 
         for i, item in enumerate(compressed_block):
             if item == self.free_character:
-                break
+                continue
 
             checksum += i * item
 
@@ -82,13 +157,20 @@ class Day9Solver:
         print(f"{in_file_name}: checksum {checksum}")
 
     def solve_part_2(self, in_file_name):
-        pass
+        block_expansion = self.create_block_expansion(in_file_name)
+        compressed_block = self.compress_blocks_with_whole_files(block_expansion)
+        checksum = self.filesystem_checksum(compressed_block)
+
+        print(f"{in_file_name}: checksum {checksum}")
 
 def main():
     day9Solver = Day9Solver()
 
     day9Solver.solve_part_1("input_simple_1.txt")
     day9Solver.solve_part_1("input.txt")
+
+    day9Solver.solve_part_2("input_simple_1.txt")
+    day9Solver.solve_part_2("input.txt")
 
 if __name__ == "__main__":
     main()
